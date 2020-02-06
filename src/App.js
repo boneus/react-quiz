2d172eb5a5d58bb7347b969c159ca1afc35b0923
Stanislav Tiptsov <4terion@gmail.com>
2020-02-07 01:26:17 +0300

    Create main page (Quiz container)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react'
 import Layout from './hoc/Layout/Layout'
+import Quiz from './containers/Quiz/Quiz'
 
 class App extends React.Component {
   render() {
     return (
       <Layout>
-
-        <div style={{ width: 400, border: '1px solid black' }}>
-          <h1>Layout works</h1>
-        </div>
+        <Quiz />
       </Layout>
     );
   }
