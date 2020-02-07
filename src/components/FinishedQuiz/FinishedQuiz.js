7e8177dab6f0db8296f471d8449a4fedb93cca1f
Stanislav Tiptsov <4terion@gmail.com>
2020-02-08 00:39:38 +0300

    Return to quizzes list using link component

--- src/components/FinishedQuiz/FinishedQuiz.js
+++ src/components/FinishedQuiz/FinishedQuiz.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Link} from "react-router-dom"
 import classes from './FinishedQuiz.module.css'
 import Button from "../UI/Button/Button"
 
@@ -36,7 +37,9 @@ const FinishedQuiz = props => {
 
       <div>
         <Button onClick={props.onRetry} type="primary">Повторить</Button>
-        <Button type="success">Перейти в список тестов</Button>
+        <Link to="/">
+          <Button type="success">Перейти в список тестов</Button>
+        </Link>
       </div>
     </div>
   )
