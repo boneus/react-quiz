e7a6af59fe13656bf539d54c61ae9e42689ddb17
Stanislav Tiptsov <4terion@gmail.com>
2020-02-07 17:45:02 +0300

    Add right\wrong answer feedback

--- src/containers/Quiz/Quiz.js
+++ src/containers/Quiz/Quiz.js
@@ -5,6 +5,7 @@ import ActiveQuiz from '../../components/ActiveQuiz/ActiveQuiz'
 class Quiz extends Component {
   state = {
     activeQuestion: 0,
+    answerState: null, // {[id]: 'success' 'error'}
     quiz: [
       {
         id: 1,
@@ -33,9 +34,36 @@ class Quiz extends Component {
 
   onAnswerClickHandler = (answerId) => {
     console.log('Answer ID: ', answerId)
-    this.setState({
-      activeQuestion: this.state.activeQuestion + 1
-    })
+
+    const question = this.state.quiz[this.state.activeQuestion]
+
+    if (question.rightAnswerId === answerId) {
+
+      this.setState({
+        answerState: {[answerId]: 'success'}
+      })
+
+      const timeout = window.setTimeout(() => {
+        if (this.isQuizFinished()) {
+          console.log('Finished')
+        } else {
+          this.setState({
+            activeQuestion: this.state.activeQuestion + 1,
+            answerState: null
+          })
+        }
+
+        window.clearTimeout(timeout)
+      }, 1000)
+    } else {
+      this.setState({
+        answerState: {[answerId]: 'error'}
+      })
+    }
+  }
+
+  isQuizFinished() {
+    return this.state.activeQuestion + 1 === this.state.quiz.length
   }
 
   render() {
@@ -49,6 +77,7 @@ class Quiz extends Component {
             onAnswerClick={this.onAnswerClickHandler}
             quizLength={this.state.quiz.length}
             answerNumber={this.state.activeQuestion + 1}
+            state={this.state.answerState}
           />
         </div>
       </div>
