0fec41dc32315263988dafe627e444ff3d673ce8
Stanislav Tiptsov <4terion@gmail.com>
2020-02-07 20:10:31 +0300

    Create MenuToggle component

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,11 +1,26 @@
 import React from 'react'
 import classes from './Layout.module.css'
+import MenuToggle from "../../components/Navigation/MenuToggle/MenuToggle"
 
 class Layout extends React.Component {
+
+  state = {
+    menu: false
+  }
+
+  onToggleMenuHandler = () => {
+    this.setState({
+      menu: !this.state.menu
+    })
+  }
+
   render() {
     return (
       <div className={classes.Layout}>
-
+        <MenuToggle
+          onToggle={this.onToggleMenuHandler}
+          isOpen={this.state.menu}
+        />
         <main>
           {this.props.children}
         </main>
