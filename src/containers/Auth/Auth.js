f7a7b01215af5cc4b2311fa673e4945e57775bd3
Stanislav Tiptsov <4terion@gmail.com>
2020-02-08 02:18:54 +0300

    Validate authorization form inputs

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,7 +1,13 @@
 import React, {Component} from 'react'
+import is from 'is_js'
 import classes from './Auth.module.css'
 import Button from "../../components/UI/Button/Button"
 import Input from "../../components/UI/Input/Input"
+
+// function validateEmail(email) {
+//   const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+//   return re.test(String(email).toLowerCase());
+// }
 
 class Auth extends Component {
 
@@ -46,8 +52,42 @@ class Auth extends Component {
     event.preventDefault()
   }
 
+  validateControl(value, validation) {
+    if (!validation) {
+      return true
+    }
+
+    let isValid = true
+
+    if (validation.required) {
+      isValid = value.trim() !== '' && isValid
+    }
+
+    if (validation.email) {
+      isValid = is.email(value) && isValid
+    }
+
+    if (validation.minLength) {
+      isValid = value.length >= validation.minLength && isValid
+    }
+
+    return isValid
+  }
+
   onChangeHandler = (event, controlName) => {
-    console.log(`${controlName}: `, event.target.value)
+    // console.log(`${controlName}: `, event.target.value)
+
+    const formControls = {...this.state.formControls}
+    formControls[controlName] = {
+      ...formControls[controlName],
+      value: event.target.value,
+      touched: true,
+      valid: this.validateControl(event.target.value, formControls[controlName].validation)
+    }
+
+    this.setState({
+      formControls
+    })
   }
 
   renderInputs() {
