fe1c01e41f69fd27fc3f53eadd1bc70f4e7fb8fe
Stanislav Tiptsov <4terion@gmail.com>
2020-02-09 22:26:41 +0300

    Create autoLogout, autoLogin

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -49,7 +49,22 @@ export function autoLogout(expiresIn) {
 }
 
 export function autoLogin() {
-
+  return async dispatch => {
+    const token = localStorage.getItem('token')
+    if (!token) {
+      dispatch(logout())
+    }
+    else {
+      const expirationDate = new Date(localStorage.getItem('expirationDate'))
+      if (expirationDate <= new Date()) {
+        dispatch(logout())
+      }
+      else {
+        dispatch(authSuccess(token))
+        dispatch(autoLogout((expirationDate.getTime() - new Date().getTime()) / 1000))
+      }
+    }
+  }
 }
 
 export function authSuccess(token) {
